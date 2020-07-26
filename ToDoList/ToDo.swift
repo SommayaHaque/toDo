191431a20660d6631a001f4de89ddf4f78c18767
Sommaya Haque <haques4@bxscience.edu>
2020-07-26 18:44:18 -0400

    My To-Do List App!

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Sommaya Haque on 6/22/20.
+//  Copyright © 2020 Sommaya Haque. All rights reserved.
+//
+
+import UIKit
+class ToDo{
+    var name = ""
+    var important = false
+}
